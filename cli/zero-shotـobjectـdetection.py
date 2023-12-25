6ac35255115dfd6bc844f172bd07a7e9c7368340
Milad Soleimani <Miladsolo1379.8.29@gmail.com>
2023-12-25 12:32:09 +0330

    change

--- cli/zero-shotـobjectـdetection.py
+++ cli/zero-shotـobjectـdetection.py
@@ -1,4 +1,5 @@
-# requierment: pip install -q transformers
+# requierment: 1 pip install -q transformers
+#              2 pip install -q tensorrt
 
 import sys
 import os
@@ -6,6 +7,8 @@ import os
 sys.path.append(os.getcwd())
 
 from PIL import Image
+from PIL import ImageDraw
+
 from transformers import pipeline
 
 import argparse
@@ -30,7 +33,18 @@ def run(conf: Dict) -> None:
         candidate_labels=candidate_labels,
     )
 
-    print(predictions)
+    draw = ImageDraw.Draw(img)
+
+    for prediction in predictions:
+        box = prediction["box"]
+        label = prediction["label"]
+        score = prediction["score"]
+
+        xmin, ymin, xmax, ymax = box.values()
+        draw.rectangle((xmin, ymin, xmax, ymax), outline="red", width=1)
+        draw.text((xmin, ymin), f"{label}: {round(score,2)}", fill="white")
+
+    img.save(conf["save_path"])
 
 
 def parse_args() -> None:
@@ -39,14 +53,21 @@ def parse_args() -> None:
     parser.add_argument(
         "--data_path",
         type=str,
-        default="dataset/tesla/images/imgs/frame_0089.jpg",
+        default="/content/eyema_OD/sampl_data/000000000025.jpg",
         help="path to data file, i.e. img.jpg",
+    )
+
+    parser.add_argument(
+        "--save_path",
+        type=str,
+        default="/content/results.jpg",
+        help="path to save data file, i.e. img.jpg",
     )
 
     parser.add_argument(
         "--candidate_labels",
         type=str,
-        default="",
+        default="animal",
         help="example: human face, star-spangled banner",
     )
 
